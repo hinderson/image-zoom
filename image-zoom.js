6cb412d8c2e4be9afeb8b4112f3282b024d1e677
Mattias Hinderson <mattias@hinderson.com>
2016-05-05 11:54:27 +0200

    Refactor module pattern to isolate instantiated constructors

--- image-zoom.js
+++ image-zoom.js
@@ -30,17 +30,7 @@
     var OFFSET = 60;
 
     // Cached values
-    var cache = {
-        ticking: false,
-        lastScrollY: window.pageYOffset,
-        viewportWidth: window.innerWidth,
-        viewportHeight: window.innerHeight
-    };
-
-    // Element states
-    var activeElems = [];
-    var currentlyZoomedIn = [];
-    var loadedImages = [];
+    var cache = {};
 
     // Window events
     var resizeEvent = utils.debounce(function ( ) {
@@ -62,32 +52,6 @@
     		cache.ticking = true;
     	}
     };
-
-    function keysPressed (e) {
-        e = e || window.event;
-
-        switch(e.which || e.keyCode) {
-            // Esc
-            case 27:
-                var currentItem = currentlyZoomedIn[currentlyZoomedIn.length - 1];
-                zoomOut(currentItem);
-                break;
-
-            // Left arrow
-            case 37:
-                e.preventDefault();
-                togglePrevImage();
-                break;
-
-            // Right arrow
-            case 39:
-                e.preventDefault();
-                toggleNextImage();
-                break;
-
-            default: return;
-        }
-    }
 
     // Transition event helper
     var transitionEvent = utils.whichTransitionEvent();
@@ -116,191 +80,222 @@
         return imgScaleFactor;
     }
 
-    function loadHighResImage (container, src) {
-        if (!(/\.(gif|jpg|jpeg|tiff|png)$/i).test(src)) {
-            return;
-        }
-
-        var image = container.querySelector('img:last-of-type');
-
-        if (loadedImages.indexOf(image) !== -1) {
-            return;
-        }
-
-        // Load high-res image
-        var highResImage = new Image();
-        highResImage.src = src;
-
-        // Remove redundant attributes
-        if (image.hasAttribute('srcset')) {
-            image.removeAttribute('srcset');
-        }
-        if (image.hasAttribute('sizes')) {
-            image.removeAttribute('sizes');
-        }
-
-        highResImage.onload = function ( ) {
-            loadedImages.push(image);
-            image.src = src;
-            pubsub.publish('imageLoaded', image);
-        };
-    }
-
-    function zoomOut (container, callback) {
-        pubsub.publish('zoomOutStart', container);
-
-        // Remove keyboard commands
-        window.removeEventListener('keydown', keysPressed);
-
-        // Reset transforms
-        utils.requestAnimFrame.call(window, function ( ) {
-            container.classList.remove('is-zoomed');
-            container.isAnimating = true;
-            container.style.msTransform = '';
-            container.style.webkitTransform = '';
-            container.style.transform = '';
-        });
-
-        // Wait for transition to end
-        container.addEventListener(transitionEvent, function resetImage ( ) {
-            container.removeEventListener(transitionEvent, resetImage);
-            container.classList.remove('is-active');
-            container.isAnimating = false;
-            pubsub.publish('zoomOutEnd', container);
-
-            var i = currentlyZoomedIn.indexOf(container);
-            if (i != -1) { currentlyZoomedIn.splice(i, 1); }
-            if (callback) { callback(); }
-        });
-    }
-
-    function zoomIn (container, callback) {
-        var thumbRect = container.getBoundingClientRect();
-        var imageRect = {
-            width: container.getAttribute('data-width'),
-            height: container.getAttribute('data-height'),
-        };
-
-        pubsub.publish('zoomInStart', container);
-
-        container.classList.add('is-active');
-
-        // Force repaint
-        var repaint = container.offsetWidth;
-
-        // Calculate offset
-        var viewportY = cache.viewportHeight / 2;
-        var viewportX = cache.viewportWidth / 2;
-        var imageCenterY = thumbRect.top + (thumbRect.height / 2);
-        var imageCenterX = thumbRect.left + (thumbRect.width / 2);
-        var translate = 'translate3d(' + (viewportX - imageCenterX) + 'px, ' + (viewportY - imageCenterY) + 'px, 0)';
-
-        // Calculate scale ratio
-        var scale = 'scale(' + calculateZoom(imageRect, thumbRect) + ')';
-
-        // Apply transforms
-        utils.requestAnimFrame.call(window, function ( ) {
-            container.classList.add('is-zooming');
-            container.isAnimating = true;
-
-            container.style.msTransform = translate + ' ' + scale;
-            container.style.webkitTransform = translate + ' ' + scale;
-            container.style.transform = translate + ' ' + scale;
-        });
-
-        // Events
-        window.addEventListener('keydown', keysPressed);
-
-        // Wait for transition to end
-        container.addEventListener(transitionEvent, function activateImage ( ) {
-            container.removeEventListener(transitionEvent, activateImage);
-
-            container.classList.remove('is-zooming');
-            container.classList.add('is-zoomed');
-            container.isAnimating = false;
-            currentlyZoomedIn.push(container);
-            pubsub.publish('zoomInEnd', container);
-
-            loadHighResImage(container, container.getAttribute('href'));
-
-            if (callback) { callback(); }
-        });
-    }
-
-    var toggleNextImage = function ( ) {
-        var currentItem = currentlyZoomedIn[currentlyZoomedIn.length - 1];
-        var currentIndex = activeElems.indexOf(currentItem);
-
-        if ((currentIndex + 1) >= activeElems.length) {
-            return;
-        }
-
-        zoomOut(currentItem, function ( ) {
-            var nextItem = activeElems[currentIndex + 1];
-            zoomIn(nextItem);
-        });
-    };
-
-    var togglePrevImage = function ( ) {
-        var currentItem = currentlyZoomedIn[currentlyZoomedIn.length - 1];
-        var currentIndex = activeElems.indexOf(currentItem);
-
-        if (currentIndex <= 0) {
-            return;
-        }
-
-        zoomOut(currentItem, function ( ) {
-            var prevItem = activeElems[currentIndex - 1];
-            zoomIn(prevItem);
-        });
-    };
-
-    var toggleZoom = function (event) {
-        event.preventDefault();
-
-        var container = event.delegateTarget;
-
-        if (container.isAnimating) {
-            return;
-        }
-
-        if (container.classList.contains('is-zoomed')) {
-            zoomOut(container);
-        } else {
-            zoomIn(container);
-        }
-    };
-
-    var destroy = function ( ) {
-        window.removeEventListener('resize', resizeEvent);
-        window.removeEventListener('scroll', scrollEvent);
-
-        utils.forEach(activeElems, function (index, elem) {
-            elem.removeEventListener('click', elem.eventListener);
-        });
-        activeElems.length = 0;
-    };
-
+    // Constructor
     function ImageZoom (elems, options) {
         if (!elems) return;
 
-        // Call upon resizeEvent just in case the constructor is called late
-        resizeEvent();
-        window.addEventListener('resize', resizeEvent);
-        window.addEventListener('scroll', scrollEvent);
+        // Element states
+        var activeElems = [];
+        var currentlyZoomedIn = [];
+        var loadedImages = [];
 
         // Update default options
         if (options) {
             OFFSET = options.offset;
         }
 
-        // Export event emitter
-        this.on = pubsub.subscribe;
+        // Set some cache defaults
+        cache = {
+            ticking: false,
+            lastScrollY: window.pageYOffset,
+            viewportWidth: window.innerWidth,
+            viewportHeight: window.innerHeight,
+        };
 
-        // Export functions
-        this.prev = togglePrevImage;
-        this.next = toggleNextImage;
-        this.destroy = destroy;
+        window.addEventListener('resize', resizeEvent);
+        window.addEventListener('scroll', scrollEvent);
+
+        // Private functions
+        function zoomIn (container, callback) {
+            var thumbRect = container.getBoundingClientRect();
+            var imageRect = {
+                width: container.getAttribute('data-width'),
+                height: container.getAttribute('data-height'),
+            };
+
+            pubsub.publish('zoomInStart', container);
+
+            container.classList.add('is-active');
+
+            // Force repaint
+            var repaint = container.offsetWidth;
+
+            // Calculate offset
+            var viewportY = cache.viewportHeight / 2;
+            var viewportX = cache.viewportWidth / 2;
+            var imageCenterY = thumbRect.top + (thumbRect.height / 2);
+            var imageCenterX = thumbRect.left + (thumbRect.width / 2);
+            var translate = 'translate3d(' + (viewportX - imageCenterX) + 'px, ' + (viewportY - imageCenterY) + 'px, 0)';
+
+            // Calculate scale ratio
+            var scale = 'scale(' + calculateZoom(imageRect, thumbRect) + ')';
+
+            // Apply transforms
+            utils.requestAnimFrame.call(window, function ( ) {
+                container.classList.add('is-zooming');
+                container.isAnimating = true;
+
+                container.style.msTransform = translate + ' ' + scale;
+                container.style.webkitTransform = translate + ' ' + scale;
+                container.style.transform = translate + ' ' + scale;
+            });
+
+            // Events
+            window.addEventListener('keydown', keysPressed);
+
+            // Wait for transition to end
+            container.addEventListener(transitionEvent, function activateImage ( ) {
+                container.removeEventListener(transitionEvent, activateImage);
+
+                container.classList.remove('is-zooming');
+                container.classList.add('is-zoomed');
+                container.isAnimating = false;
+                currentlyZoomedIn.push(container);
+                pubsub.publish('zoomInEnd', container);
+
+                loadHighResImage(container, container.getAttribute('href'));
+
+                if (callback) { callback(); }
+            });
+        }
+
+        function zoomOut (container, callback) {
+            pubsub.publish('zoomOutStart', container);
+
+            // Remove keyboard commands
+            window.removeEventListener('keydown', keysPressed);
+
+            // Reset transforms
+            utils.requestAnimFrame.call(window, function ( ) {
+                container.classList.remove('is-zoomed');
+                container.isAnimating = true;
+                container.style.msTransform = '';
+                container.style.webkitTransform = '';
+                container.style.transform = '';
+            });
+
+            // Wait for transition to end
+            container.addEventListener(transitionEvent, function resetImage ( ) {
+                container.removeEventListener(transitionEvent, resetImage);
+                container.classList.remove('is-active');
+                container.isAnimating = false;
+                pubsub.publish('zoomOutEnd', container);
+
+                var i = currentlyZoomedIn.indexOf(container);
+                if (i != -1) { currentlyZoomedIn.splice(i, 1); }
+                if (callback) { callback(); }
+            });
+        }
+
+        function loadHighResImage (container, src, callback) {
+            if (!(/\.(gif|jpg|jpeg|tiff|png)$/i).test(src)) {
+                return;
+            }
+
+            var image = container.querySelector('img:last-of-type');
+
+            if (loadedImages.indexOf(image) !== -1) {
+                return;
+            }
+
+            // Load high-res image
+            var highResImage = new Image();
+            highResImage.src = src;
+
+            // Remove redundant attributes
+            if (image.hasAttribute('srcset')) {
+                image.removeAttribute('srcset');
+            }
+            if (image.hasAttribute('sizes')) {
+                image.removeAttribute('sizes');
+            }
+
+            highResImage.onload = function ( ) {
+                loadedImages.push(image);
+                image.src = src;
+                pubsub.publish('imageLoaded', image);
+            };
+        }
+
+        function togglePrevImage ( ) {
+            var currentItem = currentlyZoomedIn[currentlyZoomedIn.length - 1];
+            var currentIndex = activeElems.indexOf(currentItem);
+
+            if (currentIndex <= 0) {
+                return;
+            }
+
+            zoomOut(currentItem, function ( ) {
+                var prevItem = activeElems[currentIndex - 1];
+                zoomIn(prevItem);
+            });
+        }
+
+        function toggleNextImage ( ) {
+            var currentItem = currentlyZoomedIn[currentlyZoomedIn.length - 1];
+            var currentIndex = activeElems.indexOf(currentItem);
+
+            if ((currentIndex + 1) >= activeElems.length) {
+                return;
+            }
+
+            zoomOut(currentItem, function ( ) {
+                var nextItem = activeElems[currentIndex + 1];
+                zoomIn(nextItem);
+            });
+        }
+
+        function keysPressed (e) {
+            e = e || window.event;
+
+            switch(e.which || e.keyCode) {
+                // Esc
+                case 27:
+                    var currentItem = currentlyZoomedIn[currentlyZoomedIn.length - 1];
+                    zoomOut(currentItem);
+                    break;
+
+                // Left arrow
+                case 37:
+                    e.preventDefault();
+                    togglePrevImage();
+                    break;
+
+                // Right arrow
+                case 39:
+                    e.preventDefault();
+                    toggleNextImage();
+                    break;
+
+                default: return;
+            }
+        }
+
+        function destroy ( ) {
+            window.removeEventListener('resize', resizeEvent);
+            window.removeEventListener('scroll', scrollEvent);
+
+            utils.forEach(activeElems, function (index, elem) {
+                elem.removeEventListener('click', elem.eventListener);
+            });
+            activeElems.length = 0;
+        }
+
+        function toggleZoom (event) {
+            event.preventDefault();
+
+            var container = event.delegateTarget;
+
+            if (container.isAnimating) {
+                return;
+            }
+
+            if (container.classList.contains('is-zoomed')) {
+                zoomOut(container);
+            } else {
+                zoomIn(container);
+            }
+        }
 
         // Attach click event listeners to all provided elems
         var bindElem = function (elem) {
@@ -317,9 +312,17 @@
         } else if (elems) {
             bindElem(elems);
         }
+
+        // Exposed methods
+        return {
+            on: pubsub.subscribe,
+            prev: togglePrevImage,
+            next: toggleNextImage,
+            destroy: destroy,
+        };
     }
 
-	// Expose to interface
+    // Expose to interface
 	if (typeof module === 'object' && typeof module.exports === 'object') {
 		// CommonJS, just export
 		module.exports = ImageZoom;
@@ -327,5 +330,4 @@
 		// AMD support
 		define('ImageZoom', function ( ) { return ImageZoom; } );
 	}
-
 }));
